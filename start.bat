726b1044f7474f065147de372b8064ce694c32fb
pogchampjacob <79588299+pogchampjacob@users.noreply.github.com>
2021-07-20 19:04:10 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -16,6 +16,6 @@ echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: administrator
-echo Password: TheOwoGod1234
+echo Password: Password is not set. You just need the username.
 echo You can login now.
 ping -n 10 127.0.0.1 >nul
